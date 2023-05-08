a44b1094d8f60c128c77a6c28e286a98f48b331a
sweder <swestrong9@gmail.com>
2023-05-08 10:35:51 +0200

    changes

--- src/network.h
+++ src/network.h
@@ -6,6 +6,7 @@
 void InitNetwork(Network *network);
 void AddNetworkLayer(Network *network, unsigned int neuronAmount, ActivationFunction activationFunction);
 void RandomizeNetwork(Network *network);
+float LossCalc(Net)
 
 void ExucuteNetwork(Network *network);
 

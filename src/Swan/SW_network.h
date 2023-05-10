14837aef3eb312bfcb8a81c0ad4bb71faa203327
Qruintrox <82107317+Qruintrox@users.noreply.github.com>
2023-05-10 17:41:45 +0200

    Update SW_network.h
    een paar van sweders dingen fixen

--- src/Swan/SW_network.h
+++ src/Swan/SW_network.h
@@ -10,7 +10,7 @@ void SW_RandomizeNetwork(SW_Network *network);
 
 void SW_SetNetworkInput(SW_Network *network, float *input);   // input should have the same length as the first layer in the network
 
-float SW_TrainNeuralNetwork(SW_Network *network, float **input, float **correctOutput, unsigned int batchSize, float minimumLoss, SW_LossFunction LossFunction);
+void SW_TrainNeuralNetwork(SW_Network *network, float **input, float **correctOutput, unsigned int batchSize, float minimumLoss, SW_LossFunction LossFunction);
 void SW_ExucuteNetwork(SW_Network *network);
 float SW_CalculateLoss(SW_Network *network, SW_LossFunction lossFunction, float *input, float *correctOutput); // input should have the same length as the first layer in the network, and correctOutput should have the same length as the last layer in the network
 

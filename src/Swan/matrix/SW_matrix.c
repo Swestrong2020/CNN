f7f9b47aedb3bad2f692898acefce16fd2e702ff
Distractech <distractech.dev@gmail.com>
2023-05-29 21:40:10 +0200

    *does a matrix dodge*

--- src/Swan/matrix/SW_matrix.c
+++ src/Swan/matrix/SW_matrix.c
@@ -4,20 +4,18 @@
 
 #include "SW_matrix.h"
 
-void SW_initMatrixEmpty(SW_Matrix *matrix, uint32_t rows, uint32_t columns)
+void SWM_initMatrix(SWM_Matrix *matrix, uint32_t rows, uint32_t columns)
 {
     matrix->rows = rows;
     matrix->columns = columns;
 
-    matrix->data = (SW_MatrixData_t) malloc(sizeof(float) * rows);
-    for (uint32_t i = 0; i < rows; i++)
-        matrix->data[i] = malloc(sizeof(float) * columns);
+    matrix->data = SWM_createData(rows, columns);
 
     // initialize to 0
-    // SW_fillMatrix(matrix, .0f);
+    // SWM_fillMatrix(matrix, .0f);
 }
 
-void SW_initMatrixData(SW_Matrix *matrix, uint32_t rows, uint32_t columns, float **data)
+void SWM_initMatrixData(SWM_Matrix *matrix, uint32_t rows, uint32_t columns, SWM_MatrixData_t data)
 {
     matrix->rows = rows;
     matrix->columns = columns;
@@ -25,23 +23,20 @@ void SW_initMatrixData(SW_Matrix *matrix, uint32_t rows, uint32_t columns, float
     matrix->data = data;
 }
 
-void SW_destroyMatrix(SW_Matrix *matrix)
+void SWM_destroyMatrix(SWM_Matrix *matrix)
 {
-    for (int i = 0; i < matrix->rows; i++)
-        free(matrix->data[i]);
     free(matrix->data);
 }
 
-void SW_fillMatrix(SW_Matrix *matrix, float val)
+void SWM_fillMatrix(SWM_Matrix *matrix, SWM_MatrixValue_t val)
 {
-    for (int i = 0; i < matrix->rows; i++)
-    for (int j = 0; j < matrix->columns; j++)
-        matrix->data[i][j] = val;
+    for (int i = 0, l = matrix->rows * matrix->columns; i < l; i++)
+        matrix->data[i] = val;
 }
 
 // matrix operations
 
-SW_Matrix SW_addMatrix(SW_Matrix *a, SW_Matrix *b)
+SWM_Matrix SWM_addMatrix(SWM_Matrix *a, SWM_Matrix *b)
 {
     // safety guards
     if (a->rows != b->rows || a->columns != b->columns)
@@ -49,8 +44,38 @@ SW_Matrix SW_addMatrix(SW_Matrix *a, SW_Matrix *b)
         fputs("Failed to add differently sized matrices", stderr);
     }
 
-    
+    SWM_MatrixData_t outputData = SWM_createData(a->rows, a->columns);
+    SWM_Matrix out;
+    SWM_initMatrixData(&out, a->rows, a->columns, outputData);
+
+    for (uint32_t i = 0, l = a->rows; i < l; i++)
+    for (uint32_t j = 0, k = a->columns; j < k; j++)
+        SWM_set(&out, i, j, SWM_at(a, i, j) + SWM_at(b, i, j));
+
+    return out;
 }
 
-SW_Matrix SW_multiplyMatrix(SW_Matrix *a, SW_Matrix *b);
-SW_Matrix SW_multiplyScalar(SW_Matrix *a, float scalar);
+SWM_Matrix SWM_multiplyMatrix(SWM_Matrix *a, SWM_Matrix *b);
+SWM_Matrix SWM_multiplyScalar(SWM_Matrix *a, float scalar);
+
+// util
+
+void SWM_printm(SWM_Matrix *matrix)
+{
+    if (matrix->rows == 0 || matrix->columns == 0)
+    {
+        fputs("Not printing empty matrix >:(", stdout);
+    }
+    
+    fprintf(stdout, "(%d, %d)\n", matrix->rows, matrix->columns);
+
+    for (int i = 0, li = matrix->rows; i < li; i++)
+    {
+        fprintf(stdout, "[%.6f", SWM_at(matrix, i, 0));
+        
+        for (int j = 1, lj = matrix->columns; j < lj; j++)
+            fprintf(stdout, ", %.6f", SWM_at(matrix, i, j));
+        
+        fputs("]\n", stdout);
+    }
+}
--- src/Swan/matrix/SW_matrix.h
+++ src/Swan/matrix/SW_matrix.h
@@ -2,27 +2,53 @@
 #define SW_MATRIX_H
 
 #include <stdint.h>
+#include <stdlib.h>
 
 /* SW_MatrixData_t data[row][column] */
-typedef float** SW_MatrixData_t;
+typedef float* SWM_MatrixData_t;
+typedef float SWM_MatrixValue_t;
 
-typedef struct SW_Matrix
+typedef struct SWM_Matrix
 {
 
-    SW_MatrixData_t data;
+    SWM_MatrixData_t data;
     uint32_t rows, columns;
 
-} SW_Matrix;
+} SWM_Matrix;
 
-void SW_initMatrixEmpty(SW_Matrix *matrix, uint32_t rows, uint32_t columns);
-void SW_initMatrixData(SW_Matrix *matrix, uint32_t rows, uint32_t columns, float **data);
-void SW_destroyMatrix(SW_Matrix *matrix);
-void SW_fillMatrix(SW_Matrix *matrix, float val);
+static inline uint32_t SWM_index(SWM_Matrix *matrix, uint32_t row, uint32_t col)
+{
+    return (row * matrix->columns + col);
+}
+
+static inline SWM_MatrixValue_t SWM_at(SWM_Matrix *matrix, uint32_t row, uint32_t col)
+{
+    return matrix->data[SWM_index(matrix, row, col)];
+}
+
+static inline void SWM_set(SWM_Matrix *matrix, uint32_t row, uint32_t column, float value)
+{
+    matrix->data[SWM_index(matrix, row, column)] = value;
+}
+
+static inline SWM_MatrixData_t SWM_createData(uint32_t rows, uint32_t columns)
+{
+    return (SWM_MatrixData_t) malloc(sizeof(float) * rows * columns);
+}
+
+void SWM_initMatrix(SWM_Matrix *matrix, uint32_t rows, uint32_t columns);
+void SWM_initMatrixData(SWM_Matrix *matrix, uint32_t rows, uint32_t columns, SWM_MatrixData_t data);
+void SWM_destroyMatrix(SWM_Matrix *matrix);
+void SWM_fillMatrix(SWM_Matrix *matrix, float val);
 
 // matrix operations
 
-SW_Matrix SW_addMatrix(SW_Matrix *a, SW_Matrix *b);
-SW_Matrix SW_multiplyMatrix(SW_Matrix *a, SW_Matrix *b);
-SW_Matrix SW_multiplyScalar(SW_Matrix *a, float scalar);
+SWM_Matrix SWM_addMatrix(SWM_Matrix *a, SWM_Matrix *b); /* ret handled by caller */
+SWM_Matrix SWM_multiplyMatrix(SWM_Matrix *a, SWM_Matrix *b); /* ret handled by caller */
+SWM_Matrix SWM_multiplyScalar(SWM_Matrix *a, float scalar); /* ret handled by caller */
+
+// util
+
+void SWM_printm(SWM_Matrix *matrix);
 
 #endif // SW_MATRIX_H

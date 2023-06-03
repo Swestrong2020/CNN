72280794d72788e5a46e0f198714156bb33e6d4c
Distractech <distractech.dev@gmail.com>
2023-06-03 18:45:54 +0200

    more cleanup

--- src/main.c
+++ src/main.c
@@ -54,139 +54,140 @@ int main(void)
 
     // parsing the MNIST dataset
     // ------------------------------------------------
-
-
-
-    // training labels
-    FILE *fTrainingLabels = fopen("MNISTdataset/train-labels-idx1-ubyte", "rb");
-
-    if (fTrainingLabels == NULL)
+    // code block for vscode collapse, QOL
     {
-        fputs("error opening training label file\n", stderr);
-        abort();
-    }
 
-    int32_t trainingLabelsMagicBytes;
 
-    trainingLabelsMagicBytes = (int32_t)freadReverse32(fTrainingLabels);
-    nTrainingLabels = (int32_t)freadReverse32(fTrainingLabels);
+        // training labels
+        FILE *fTrainingLabels = fopen("MNISTdataset/train-labels-idx1-ubyte", "rb");
 
-    if (trainingLabelsMagicBytes != 2049 && trainingLabelsMagicBytes != 17301504)
-    {
-        fputs("training labels corrupted\n", stderr);
-        abort();
-    }
+        if (fTrainingLabels == NULL)
+        {
+            fputs("error opening training label file\n", stderr);
+            abort();
+        }
 
-    trainingLabels = realloc(trainingLabels, sizeof(uint8_t) * nTrainingLabels);
-    size_t trainingLabelsNRead = fread(trainingLabels, sizeof(uint8_t), nTrainingLabels, fTrainingLabels);
+        int32_t trainingLabelsMagicBytes;
+
+        trainingLabelsMagicBytes = (int32_t)freadReverse32(fTrainingLabels);
+        nTrainingLabels = (int32_t)freadReverse32(fTrainingLabels);
+
+        if (trainingLabelsMagicBytes != 2049 && trainingLabelsMagicBytes != 17301504)
+        {
+            fputs("training labels corrupted\n", stderr);
+            abort();
+        }
+
+        trainingLabels = realloc(trainingLabels, sizeof(uint8_t) * nTrainingLabels);
+        size_t trainingLabelsNRead = fread(trainingLabels, sizeof(uint8_t), nTrainingLabels, fTrainingLabels);
+        
+    #ifdef DEBUG
+        printf("read %zu bytes into trainingLabels\n", trainingLabelsNRead);
+    #endif
+
+        fclose(fTrainingLabels);
+
+
+
+        // training images
+        FILE *fTrainingImages = fopen("MNISTdataset/train-images-idx3-ubyte", "rb");
+
+        if (fTrainingImages == NULL)
+        {
+            fputs("error opening training images file\n", stderr);
+            abort();
+        }
+
+        int32_t trainingImagesMagicBytes, trainingImagesRows, trainingImagesColumns;
+
+        trainingImagesMagicBytes = (int32_t)freadReverse32(fTrainingImages);
+        nTrainingImages = (int32_t)freadReverse32(fTrainingImages);
+        trainingImagesRows = (int32_t)freadReverse32(fTrainingImages);
+        trainingImagesColumns = (int32_t)freadReverse32(fTrainingImages);
+
+        if (trainingImagesMagicBytes != 2051 && trainingImagesMagicBytes != 50855936)
+        {
+            fputs("training images corrupted\n", stderr);
+            abort();
+        }
+
+        trainingImages = realloc(trainingImages, sizeof(uint8_t) * nTrainingImages * trainingImagesRows * trainingImagesColumns);
+        size_t trainingImagesNRead = fread(trainingImages, sizeof(uint8_t), nTrainingImages * trainingImagesRows * trainingImagesColumns, fTrainingImages);
+
+    #ifdef DEBUG
+        printf("read %zu bytes into trainingImages\n", trainingImagesNRead);
+    #endif
+
+        fclose(fTrainingImages);
+
+
+
+        // test labels
+        FILE *fTestLabels = fopen("MNISTdataset/t10k-labels-idx1-ubyte", "rb");
+
+        if (fTestLabels == NULL)
+        {
+            fputs("error opening test label file\n", stderr);
+            abort();
+        }
+
+        int32_t testLabelsMagicBytes;
+
+        testLabelsMagicBytes = (int32_t)freadReverse32(fTestLabels);
+        nTestLabels = (int32_t)freadReverse32(fTestLabels);
+
+        if (testLabelsMagicBytes != 2049 && testLabelsMagicBytes != 17301504)
+        {
+            fputs("test labels corrupted\n", stderr);
+            abort();
+        }
+
+        testLabels = realloc(testLabels, sizeof(uint8_t) * nTestLabels);
+        size_t testLabelsNRead = fread(testLabels, sizeof(uint8_t), nTestLabels, fTestLabels);
+
+    #ifdef DEBUG
+        printf("read %zu bytes into testLabels\n", testLabelsNRead);
+    #endif
+
+        fclose(fTestLabels);
+
+
+
+        // test images
+        FILE *fTestImages = fopen("MNISTdataset/t10k-images-idx3-ubyte", "rb");
+
+        if (fTestImages == NULL)
+        {
+            fputs("error opening test images file\n", stderr);
+            abort();
+        }
+
+        int32_t testImageMagicBytes, testImagesRows, testImagesColumns;
+
+        testImageMagicBytes = (int32_t)freadReverse32(fTestImages);
+        nTestImages = (int32_t)freadReverse32(fTestImages);
+        testImagesRows = (int32_t)freadReverse32(fTestImages);
+        testImagesColumns = (int32_t)freadReverse32(fTestImages);
+
+        if (testImageMagicBytes != 2051 && testImageMagicBytes != 50855936)
+        {
+            fputs("test images corrupted\n", stderr);
+            abort();
+        }
+
+        testImages = realloc(testImages, sizeof(uint8_t) * nTestImages * testImagesRows * testImagesColumns);
+        size_t testImagesNRead = fread(testImages, sizeof(uint8_t), nTestImages * testImagesRows * testImagesColumns, fTestImages);
+
+    #ifdef DEBUG
+        printf("read %zu bytes into testImages\n", testImagesNRead);
+    #endif
+
+
+        fclose(fTestImages);
+
+
     
-#ifdef DEBUG
-    printf("read %zu bytes into trainingLabels\n", trainingLabelsNRead);
-#endif
-
-    fclose(fTrainingLabels);
-
-
-
-    // training images
-    FILE *fTrainingImages = fopen("MNISTdataset/train-images-idx3-ubyte", "rb");
-
-    if (fTrainingImages == NULL)
-    {
-        fputs("error opening training images file\n", stderr);
-        abort();
     }
-
-    int32_t trainingImagesMagicBytes, trainingImagesRows, trainingImagesColumns;
-
-    trainingImagesMagicBytes = (int32_t)freadReverse32(fTrainingImages);
-    nTrainingImages = (int32_t)freadReverse32(fTrainingImages);
-    trainingImagesRows = (int32_t)freadReverse32(fTrainingImages);
-    trainingImagesColumns = (int32_t)freadReverse32(fTrainingImages);
-
-    if (trainingImagesMagicBytes != 2051 && trainingImagesMagicBytes != 50855936)
-    {
-        fputs("training images corrupted\n", stderr);
-        abort();
-    }
-
-    trainingImages = realloc(trainingImages, sizeof(uint8_t) * nTrainingImages * trainingImagesRows * trainingImagesColumns);
-    size_t trainingImagesNRead = fread(trainingImages, sizeof(uint8_t), nTrainingImages * trainingImagesRows * trainingImagesColumns, fTrainingImages);
-
-#ifdef DEBUG
-    printf("read %zu bytes into trainingImages\n", trainingImagesNRead);
-#endif
-
-    fclose(fTrainingImages);
-
-
-
-    // test labels
-    FILE *fTestLabels = fopen("MNISTdataset/t10k-labels-idx1-ubyte", "rb");
-
-    if (fTestLabels == NULL)
-    {
-        fputs("error opening test label file\n", stderr);
-        abort();
-    }
-
-    int32_t testLabelsMagicBytes;
-
-    testLabelsMagicBytes = (int32_t)freadReverse32(fTestLabels);
-    nTestLabels = (int32_t)freadReverse32(fTestLabels);
-
-    if (testLabelsMagicBytes != 2049 && testLabelsMagicBytes != 17301504)
-    {
-        fputs("test labels corrupted\n", stderr);
-        abort();
-    }
-
-    testLabels = realloc(testLabels, sizeof(uint8_t) * nTestLabels);
-    size_t testLabelsNRead = fread(testLabels, sizeof(uint8_t), nTestLabels, fTestLabels);
-
-#ifdef DEBUG
-    printf("read %zu bytes into testLabels\n", testLabelsNRead);
-#endif
-
-    fclose(fTestLabels);
-
-
-
-    // test images
-    FILE *fTestImages = fopen("MNISTdataset/t10k-images-idx3-ubyte", "rb");
-
-    if (fTestImages == NULL)
-    {
-        fputs("error opening test images file\n", stderr);
-        abort();
-    }
-
-    int32_t testImageMagicBytes, testImagesRows, testImagesColumns;
-
-    testImageMagicBytes = (int32_t)freadReverse32(fTestImages);
-    nTestImages = (int32_t)freadReverse32(fTestImages);
-    testImagesRows = (int32_t)freadReverse32(fTestImages);
-    testImagesColumns = (int32_t)freadReverse32(fTestImages);
-
-    if (testImageMagicBytes != 2051 && testImageMagicBytes != 50855936)
-    {
-        fputs("test images corrupted\n", stderr);
-        abort();
-    }
-
-    testImages = realloc(testImages, sizeof(uint8_t) * nTestImages * testImagesRows * testImagesColumns);
-    size_t testImagesNRead = fread(testImages, sizeof(uint8_t), nTestImages * testImagesRows * testImagesColumns, fTestImages);
-
-#ifdef DEBUG
-    printf("read %zu bytes into testImages\n", testImagesNRead);
-#endif
-
-
-    fclose(fTestImages);
-
-
-
-    
     // ------------------------------------------------
 
 #ifdef DEBUG
